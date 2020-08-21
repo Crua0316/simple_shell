976836e77cc36a9138bc977611450b58c30e12b1
Crua0316 <cresrugi@gmail.com>
2020-08-21 00:12:52 +0000

    _Shell, minishell

--- _shell.c
+++ _shell.c
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include "holberton.h"
 /**
  * main - executes initial functions to run the mini shell
@@ -15,9 +10,14 @@ int main(void)
 	while (1)
 	{
 		buffer = prompt();
+		if (!buffer)
+			printf("hello");
 		argu = tokens(buffer);
+		check_input(argu);
+		argu[0] = _inicio(argu[0]);
 		_execute(argu);
 		free_all(2, buffer, argu);
+		/*free(argu[0]);*/
 	}
 	return (0);
 }
